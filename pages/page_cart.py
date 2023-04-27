3de783d3866c5750905f931a5514440caa01288a
Aliaksandr Minkevich <minkeviiich@gmail.com>
2023-04-27 23:11:34 +0300

    Ver.2

--- pages/page_cart.py
+++ pages/page_cart.py
@@ -14,3 +14,23 @@ class FormPageCart(BasePage):
         result = self.element_is_visible(Locators.BASKET_TEXT)
         result = result.text
         return result
+
+    def fields_cart_several(self):
+        button = self.element_is_visible(Locators.FLOWERS_TWO)
+        self.element_is_not_visible(button)
+        self.element_is_visible(Locators.FLOWERS_SEVERAL_CLICK).click()
+        self.element_is_visible(Locators.FLOWERS_SEVERAL_CLICK).click()
+        self.element_is_visible(Locators.FLOWERS_CLICK_SEVERAL).click()
+
+    def fields_cart_several_two(self):
+        button = self.element_is_visible(Locators.FLOWERS_TWO)
+        self.element_is_not_visible(button)
+        self.element_is_visible(Locators.FLOWERS_SEVERAL_CLICK).click()
+        self.element_is_visible(Locators.FLOWERS_SEVERAL_CLICK).click()
+        self.element_is_visible(Locators.FLOWERS_SEVERAL_CLICK).click()
+        self.element_is_visible(Locators.FLOWERS_CLICK_SEVERAL).click()
+
+    def result_cart_several(self):
+        result = self.element_is_visible(Locators.BASKET_TEXT_NUMBER)
+        result = result.text
+        return result
